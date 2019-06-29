9b68b95b592dc8cf43c4f67663ab1ec1faaa0a97
MixedFeelingsSoftwares <35192405+MixedFeelingsSoftwares@users.noreply.github.com>
2019-06-29 12:05:04 +0200

    .

---
+++ WoW Coffee Launcher/Core/Downloader/Patcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoW_Coffee_Launcher.Core.Downloader
+{
+    class Patcher
+    {
+        public const string PatchesURL = "https://www.dropbox.com/s/t3e7bz6u6axfpmx/patches.json?dl=1";
+
+        public static void DownloadPatches()
+        {
+            Task.Run(async() =>
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    string json = await wc.DownloadStringTaskAsync(PatchesURL);
+                }
+            });
+        }
+    }
+}
---
+++ WoW Coffee Launcher/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WoW_Coffee_Launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        public async void Test()
+        {
+            await Task.Run(async() =>
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    await PB_DownloadProgress.Dispatcher.BeginInvoke(new Action(() => PB_DownloadProgress.SetPercent(i, 0.5f)));
+                    await Task.Delay(100);
+                }
+            });
+        }
+
+        private void Btn_Patcher_Click(object sender, RoutedEventArgs e)
+        {
+            Test();
+        }
+    }
+
+    public static class ProgressBarExtensions
+    {
+
+        public static void SetPercent(this System.Windows.Controls.ProgressBar progressBar, double percentage, double _duration)
+        {
+            var duration = TimeSpan.FromSeconds(_duration);
+            DoubleAnimation animation = new DoubleAnimation(percentage, duration);
+
+            progressBar.BeginAnimation(System.Windows.Controls.ProgressBar.ValueProperty, animation);
+        }
+    }
+}
