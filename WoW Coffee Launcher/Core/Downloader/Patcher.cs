0a269d609e1a928aaa88f133e08ed3a2d4f68564
MixedFeelingsSoftwares <35192405+MixedFeelingsSoftwares@users.noreply.github.com>
2019-07-01 13:20:27 +0200

    .

--- WoW Coffee Launcher/Core/Downloader/Patcher.cs
+++ WoW Coffee Launcher/Core/Downloader/Patcher.cs
@@ -1,9 +1,16 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace WoW_Coffee_Launcher.Core.Downloader
 {
@@ -11,15 +18,190 @@ namespace WoW_Coffee_Launcher.Core.Downloader
     {
         public const string PatchesURL = "https://www.dropbox.com/s/t3e7bz6u6axfpmx/patches.json?dl=1";
 
+        public static string GetDataPath { get { return $"{Environment.CurrentDirectory}/Data/"; } }
+
         public static void DownloadPatches()
         {
             Task.Run(async() =>
             {
+                State.CurrentState = State.state.Deleting;
                 using (WebClient wc = new WebClient())
                 {
                     string json = await wc.DownloadStringTaskAsync(PatchesURL);
+
+                    Patches patch = JsonConvert.DeserializeObject<Patches>(json);
+                    Installer.DeletePreviousPatch(new Action(() => 
+                    {
+                        State.CurrentState = State.state.Installing;
+                        patch.Install();
+                        patch.SetRealmlist();
+                    }));
                 }
             });
         }
+
+        public static async Task<string> GetRealmlistIP()
+        {
+            return await Task.Run(async () =>
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    string json = await wc.DownloadStringTaskAsync(PatchesURL);
+
+                    Patches patch = JsonConvert.DeserializeObject<Patches>(json);
+                    return patch.Realmlist;
+                }
+            });
+        }
+    }
+
+    public static class State
+    {
+        private static state cState = state.Default;
+        public static event System.EventHandler<state> OnStateChanged;
+
+        public static state CurrentState
+        {
+            get
+            {
+                return cState;
+            }
+            set
+            {
+                cState = value;
+                StateChange();
+            }
+        }
+
+        private static void StateChange()
+        {
+            OnStateChanged?.Invoke(null, cState);
+        }
+
+        public enum state
+        {
+            Default = 0,
+            Deleting = 1,
+            Installing = 2,
+            Patching = 3,
+            Ready = 4,
+        }
+    }
+
+    public static class Installer
+    {
+        public static ProgressBar downloadBar = null;
+
+        public static async void Install(this Patches patch)
+        {
+            if (patch == null) { return; }
+            for (int i = 0; i < patch.patches.Count; i++)
+            {
+                Patch pth = patch.patches[i];
+                await Task.Run(async() =>
+                {
+                    using (WebClient wc = new WebClient())
+                    {
+                        wc.DownloadProgressChanged += (s, g) =>
+                        {
+                            downloadBar?.Dispatcher.BeginInvoke(new Action(() => downloadBar.SetPercent(g.ProgressPercentage, 1f)));
+                        };
+                        await wc.DownloadFileTaskAsync(pth.Url, $"{Patcher.GetDataPath}{pth.Name}");
+                        Debug.WriteLine(pth.Name);
+                    }
+                });
+            }
+            State.CurrentState = State.state.Ready;
+        }
+
+        public static void SetRealmlist(this Patches patch)
+        {
+            string path = $"{Environment.CurrentDirectory}/data/";
+            List<string> realmlist = Directory.EnumerateFiles(path, "*.wtf", SearchOption.AllDirectories).ToList();
+            foreach (string lst in realmlist.FindAll(x => x.Contains("realmlist")))
+            {
+                File.WriteAllText(lst, $"set realmlist {patch.Realmlist}");
+            }
+        }
+
+        public static void DeletePreviousPatch(Action callback)
+        {
+            if (isWoWDiretory())
+            {
+                List<string> datas = Directory.EnumerateFiles($"{Environment.CurrentDirectory}/Data/", "*.MPQ", SearchOption.TopDirectoryOnly).ToList();
+                foreach (string dat in datas)
+                {
+                    string name = Path.GetFileNameWithoutExtension(dat);
+
+                    bool matches = Regex.IsMatch(name, @"(Patch-)[A-Za-z]");
+                    if (matches)
+                    {
+                        Debug.WriteLine($"Name: {name} - {matches}");
+                        File.Delete(dat);
+                    }
+                }
+                callback();
+            }
+        }
+
+        public static bool isWoWDiretory()
+        {
+            return File.Exists($"{Environment.CurrentDirectory}/Wow.exe") && Directory.Exists($"{Environment.CurrentDirectory}/Data/");
+        }
+
+        public static bool isWoWDiretory(this string path)
+        {
+            return File.Exists($"{path}/Wow.exe") && Directory.Exists($"{path}/Data/");
+        }
+    }
+
+    public static class Interface
+    {
+        public static ColorAnimation AnimationByStatus(this ContentControl ctrl, bool status)
+        {
+            ColorAnimation cAnim = null;
+
+            switch (status)
+            {
+                case true:
+                    cAnim = new ColorAnimation()
+                    {
+                        From = ((SolidColorBrush)ctrl.Foreground).Color,
+                        To = (Color)ColorConverter.ConvertFromString("#FF4AC345"),
+                        Duration = TimeSpan.FromSeconds(1),
+                    };
+                    break;
+
+                case false:
+                    cAnim = new ColorAnimation()
+                    {
+                        From = ((SolidColorBrush)ctrl.Foreground).Color,
+                        To = (Color)ColorConverter.ConvertFromString("#FF911818"),
+                        Duration = TimeSpan.FromSeconds(1),
+                    };
+                    break;
+            }
+            return cAnim;
+        }
+    }
+
+    public class Patch
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+
+    public class Patches
+    {
+        [JsonProperty("realmlist")]
+        public string Realmlist { get; set; }
+
+        [JsonProperty("ver")]
+        public string Version { get; set; }
+
+        public List<Patch> patches { get; set; }
     }
 }
--- WoW Coffee Launcher/MainWindow.xaml.cs
+++ WoW Coffee Launcher/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,6 +18,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WoW_Coffee_Launcher.Core.Downloader;
 
 namespace WoW_Coffee_Launcher
 {
@@ -31,7 +35,7 @@ namespace WoW_Coffee_Launcher
 
         public async void Test()
         {
-            await Task.Run(async() =>
+            await Task.Run(async () =>
             {
                 for (int i = 0; i <= 100; i++)
                 {
@@ -43,7 +47,138 @@ namespace WoW_Coffee_Launcher
 
         private void Btn_Patcher_Click(object sender, RoutedEventArgs e)
         {
-            Test();
+            switch (State.CurrentState)
+            {
+                case State.state.Deleting:
+                    break;
+                case State.state.Installing:
+                    break;
+                case State.state.Patching:
+                    break;
+                case State.state.Default:
+                    Patcher.DownloadPatches();
+                    break;
+                case State.state.Ready:
+                    Process.Start($"{Environment.CurrentDirectory}/Wow.exe");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void FrmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            State.OnStateChanged += State_OnStateChanged;
+            Installer.downloadBar = PB_DownloadProgress;
+        }
+
+        private void State_OnStateChanged(object sender, State.state e)
+        {
+            int num = (int)e;
+            double percent = (double)num / (typeof(State.state).GetEnumValues().Length - 1) * 100;
+
+            PB_DownloadProgress.Dispatcher.BeginInvoke(new Action(() => PB_DownloadProgress.SetPercent(percent, 0.5f)));
+            if (e == State.state.Ready || e == State.state.Default)
+            {
+                switch (e)
+                {
+                    case State.state.Default:
+                        btn_Patcher.Dispatcher.BeginInvoke(new Action(() => btn_Patcher.Content = "Download Patches"));
+                        break;
+                    case State.state.Ready:
+                        btn_Patcher.Dispatcher.BeginInvoke(new Action(() => btn_Patcher.Content = "Launch WoW"));
+                        break;
+                }
+
+                btn_Patcher.Dispatcher.BeginInvoke(new Action(() => btn_Patcher.IsEnabled = true));
+            }
+            else
+            {
+                btn_Patcher.Dispatcher.BeginInvoke(new Action(() => btn_Patcher.Content = e));
+                btn_Patcher.Dispatcher.BeginInvoke(new Action(() => btn_Patcher.IsEnabled = false));
+            }
+        }
+
+        private void Txt_path_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Environment.CurrentDirectory.isWoWDiretory())
+            {
+                txt_path.Text = Environment.CurrentDirectory;
+            }
+        }
+
+        private void LB_ServerStatus_Loaded(object sender, RoutedEventArgs e)
+        {
+            PingInterval();
+        }
+
+        public void PingInterval()
+        {
+            SendPing();
+            {
+                System.Timers.Timer tmr = new System.Timers.Timer();
+                tmr.Interval = 10000;
+                tmr.Elapsed += (s, g) =>
+                {
+                    SendPing();
+                };
+                tmr.AutoReset = true;
+                tmr.Start();
+            }
+        }
+
+        public async void SendPing()
+        {
+            try
+            {
+                string IP = await Patcher.GetRealmlistIP();
+
+                bool serverStatus = await PingHost(IP, 8085, 5000);
+                string status = serverStatus ? "Online" : "Offline";
+
+                await LB_ServerStatus.Dispatcher.BeginInvoke(new Action(() => LB_ServerStatus.Content = status));
+
+
+                await Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    Console.WriteLine(status);
+                     LB_ServerStatus.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, LB_ServerStatus.AnimationByStatus(serverStatus));
+
+
+                     Icon_StatusGlobe.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, LB_ServerStatus.AnimationByStatus(serverStatus));
+                }));
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        /// <summary>
+        /// Ping Host IP address
+        /// </summary>
+        /// <param name="hostUri">Address</param>
+        /// <param name="portNumber">Port Number</param>
+        /// <param name="timeout">Timeout</param>
+        /// <returns>Ping Success</returns>
+        public static async Task<bool> PingHost(string hostUri, int portNumber, int timeout)
+        {
+            return await Task.Run(() =>
+            {
+                var client = new TcpClient();
+                var result = client.BeginConnect(hostUri, portNumber, null, null);
+
+                var success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromMilliseconds(timeout));
+
+                if (success)
+                {
+                    // we have connected
+                    client.EndConnect(result);
+                }
+
+                return success;
+            });
         }
     }
 
